e44fec517b88534a6da71a7c46afb62f41d6177c
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-01-20 20:28:45 -0300

    change the api way to work

--- pages/api/cmc.js
+++ pages/api/cmc.js
@@ -1,7 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 var axios = require('axios');
-export default function handler(req, res) {
+export default async (req, res)=> {
 
   var config = {
     method: 'get',
