000682de54d60ce1b308e0d75ebb1a00a80d44d7
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-01-20 19:57:59 -0300

    Add inital code

---
+++ pages/api/cmc.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+var axios = require('axios');
+export default function handler(req, res) {
+
+
+  var config = {
+    method: 'get',
+    url: `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${req.query.id}`,
+    headers: {
+      'X-CMC_PRO_API_KEY': process.env.API_KEY,
+      'Accept': 'application/json'
+    }
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
